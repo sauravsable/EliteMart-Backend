3af488dccccbc68b432486471a0e466a7360ddd5
sauravsable <sauravsable4102@gmail.com>
2024-06-10 15:38:24 +0530

    options changes

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -7,7 +7,10 @@ const sendToken = (user,statusCode,res)=>{
     // options for cookie
     const options = {
         maxAge: process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000,
-        httpOnly: true
+        httpOnly: true,
+        secure: 'production',
+        sameSite: 'Strict',
+        path: '/'
     };
 
     res.status(statusCode).cookie('token', token,options).json({
